0cc3579a418d016338b3946e276b1688c07d9cb2
LifeTimeScriptKiddie <kwaky808@gmail.com>
2024-06-14 10:29:15 -0400

    updates

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById('githubPageLink').addEventListener('click', function() {
     displayContent('githubPageContent');
 });
 document.getElementById('note1Link').addEventListener('click', function() {
-    fetchContent('https://raw.githubusercontent.com/LifeTimeScriptKiddie/LifeTimeScriptKiddie.github.io/main/notes/scanner.py', 'note1Content', 'note1Text');
+    fetchContent('https://raw.githubusercontent.com/LifeTimeScriptKiddie/LifeTimeScriptKiddie.github.io/main/notes/scanner.md', 'note1Content', 'note1Text');
 });
 document.getElementById('note2Link').addEventListener('click', function() {
     fetchContent('https://raw.githubusercontent.com/LifeTimeScriptKiddie/LifeTimeScriptKiddie.github.io/main/README.md', 'note2Content', 'note2Text');
