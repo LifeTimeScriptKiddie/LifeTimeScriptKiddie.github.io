c87fa9b76aba84034c65ff963a29bfe5755ba1e2
LifeTimeScriptKiddie <kwaky808@gmail.com>
2024-06-20 19:40:01 -0400

    update

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
 document.getElementById('readmeLink').addEventListener('click', function() {
     fetchContent('https://raw.githubusercontent.com/LifeTimeScriptKiddie/LifeTimeScriptKiddie.github.io/main/README.md', 'mainText');
 });
-document.getElementById('note1Link').addEventListener('click', function() {
-    fetchContent('https://raw.githubusercontent.com/LifeTimeScriptKiddie/LifeTimeScriptKiddie.github.io/main/notes/scanner.md', 'mainText');
-});
-document.getElementById('note2Link').addEventListener('click', function() {
-    fetchContent('https://raw.githubusercontent.com/LifeTimeScriptKiddie/LifeTimeScriptKiddie.github.io/main/notes/SOP%20and%20CORS.md', 'mainText');
-});
-document.getElementById('note3Link').addEventListener('click', function() {
-    fetchContent('https://raw.githubusercontent.com/LifeTimeScriptKiddie/LifeTimeScriptKiddie.github.io/main/Prototype/Reference%20-%20Prototype%20pollution.md', 'mainText');
-});
 
 function fetchContent(url, textId) {
     console.log(`Fetching content from: ${url}`);
@@ -22,12 +13,29 @@ function fetchContent(url, textId) {
         })
         .then(data => {
             document.getElementById(textId).innerHTML = marked.parse(data);
+            highlightCodeBlocks();  // Function to highlight code blocks
+            displayContent(textId);
             console.log(`Fetched and displayed content for: ${textId}`);
         })
         .catch(error => {
             console.error('Error loading content:', error);
             document.getElementById(textId).textContent = 'Error loading content';
         });
+}
+
+function displayContent(contentId) {
+    var contents = document.querySelectorAll('.content-section');
+    contents.forEach(function(content) {
+        content.style.display = 'none';
+    });
+    document.getElementById(contentId).style.display = 'block';
+    console.log(`Displayed content: ${contentId}`);
+}
+
+function highlightCodeBlocks() {
+    document.querySelectorAll('pre code').forEach((block) => {
+        hljs.highlightBlock(block);
+    });
 }
 
 // Load README.md on page load
